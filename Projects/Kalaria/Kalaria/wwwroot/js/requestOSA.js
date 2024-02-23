a8723066c5b4cbb67c6cee4abf04584047edc64c
Nicobm1999 <nicolasbanosmartins@gmail.com>
2024-02-23 17:34:56 +0100

    requestOSA added

---
+++ Projects/Kalaria/Kalaria/wwwroot/js/requestOSA.js
@@ -0,0 +1,12 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    let subjectsString = document.getElementById('subject-string');
+    let moreInfo = document.getElementById('more-info');
+    let subjects = document.getElementsByClassName('check-subject');
+    moreInfo.addEventListener('click', function () {
+        for (let i = 0; i < subjects.length; i++) {
+            if (subjects[i].checked) {
+                subjectsString.textContent += "[" + subjects[i].nextElementSibling.textContent + "]";
+            }
+        }
+    });
+});
