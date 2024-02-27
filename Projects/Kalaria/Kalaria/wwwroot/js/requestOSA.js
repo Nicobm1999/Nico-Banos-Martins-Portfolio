6ed540a0fce9b2e2b76e848562f38fd42e2f6b68
Nicobm1999 <nicolasbanosmartins@gmail.com>
2024-02-27 19:22:24 +0100

    .

--- Projects/Kalaria/Kalaria/wwwroot/js/requestOSA.js
+++ Projects/Kalaria/Kalaria/wwwroot/js/requestOSA.js
@@ -1,12 +1,33 @@
 ﻿document.addEventListener('DOMContentLoaded', function () {
     let subjectsString = document.getElementById('subject-string');
-    let moreInfo = document.getElementById('more-info');
     let subjects = document.getElementsByClassName('check-subject');
-    moreInfo.addEventListener('click', function () {
-        for (let i = 0; i < subjects.length; i++) {
+    let subjectsChecked = [];
+    for (let i = 0; i < subjects.length; i++) {
+        subjects[i].addEventListener('click', function () {
+            subjectsString.textContent = "";
             if (subjects[i].checked) {
-                subjectsString.textContent += "[" + subjects[i].nextElementSibling.textContent + "]";
+                subjectsChecked.push(subjects[i].value);
+            } else {
+                let index = subjectsChecked.indexOf(subjects[i].value);
+                if (index !== -1) {
+                    subjectsChecked.splice(index, 1);
+                }
             }
-        }
+            subjectsString.textContent = subjectsChecked.toString();
+        });
+    }
+
+  let courseString = document.getElementById('course-string');
+    let select = document.querySelector('select');
+
+    select.addEventListener('change', function () {
+        let selectedValue = select.value;
+        
+        // Limpiar el contenido actual antes de agregar el nuevo curso
+        courseString.textContent = "";
+
+        // Mostrar el valor seleccionado
+        courseString.textContent = selectedValue;
     });
 });
+
