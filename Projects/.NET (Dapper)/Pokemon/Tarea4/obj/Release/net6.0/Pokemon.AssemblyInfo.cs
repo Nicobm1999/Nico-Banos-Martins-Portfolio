a10af611e4c494acc66f2ec9d75aecc5b8332814
Nicobm1999 <nicolasbanosmartins@gmail.com>
2024-03-01 15:01:35 +0100

    .-

--- Projects/.NET (Dapper)/Pokemon/Tarea4/obj/Release/net6.0/Pokemon.AssemblyInfo.cs
+++ Projects/.NET (Dapper)/Pokemon/Tarea4/obj/Release/net6.0/Pokemon.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Kalaria")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Pokemon")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Kalaria")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Kalaria")]
+[assembly: System.Reflection.AssemblyProductAttribute("Pokemon")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Pokemon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
--- Projects/.NET (Dapper)/Pokemon/Tarea4/obj/Release/net6.0/Pokemon.RazorAssemblyInfo.cs
+++ Projects/.NET (Dapper)/Pokemon/Tarea4/obj/Release/net6.0/Pokemon.RazorAssemblyInfo.cs
@@ -11,13 +11,8 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Kalaria")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Kalaria")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Kalaria")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ProvideApplicationPartFactoryAttribute("Microsoft.AspNetCore.Mvc.ApplicationParts.ConsolidatedAssemblyApplicationPartFact" +
+    "ory, Microsoft.AspNetCore.Mvc.Razor")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
 
